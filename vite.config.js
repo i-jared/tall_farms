6999487a84815876d5f3ebe019d1b5e309f4c5c8
Jared Lambert <jaredclambert@gmail.com>
2025-02-03 09:48:02 -0600

    src

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   publicDir: 'public',
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    // Warm up frequently used files for better performance
+    warmup: {
+      clientFiles: [
+        './src/grow.js',
+        './messages.js'
+      ],
+    }
+  }
 }) 
